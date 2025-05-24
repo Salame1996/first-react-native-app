5b20fb2935fb41a7c8cedb6548f874603b1485ca
Carlos Salame <carlosandressalame@hotmail.com>
2025-05-24 01:13:26 -0500

    first commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class profile extends Component {
+  render() {
+    return (
+      <View>
+        <Text>profile</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class saved extends Component {
+  render() {
+    return (
+      <View>
+        <Text>saved</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
