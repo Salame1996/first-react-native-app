c88ad5a3ee49d357e98050f75cf34491aaed624e
Carlos Salame <carlosandressalame@hotmail.com>
2025-05-24 02:27:59 -0500

    Added some changes to bottom nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,44 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
+import { images } from "@/assets/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
 function _layout() {
   return (
     <Tabs>
-        <Tabs.Screen name="index" options={{ title: "Home", headerShown: false }} />
-        <Tabs.Screen name="profile" options={{ title: "Profile", headerShown: false }} />
-        <Tabs.Screen name="saved" options={{ title: "Saved", headerShown: false }} />
-        <Tabs.Screen name="search" options={{ title: "Search", headerShown: false }} />
-    
-        
-      </Tabs>
-
-      
-  )
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          TabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground source={images.highlight}>
+                {" "}
+                <Image
+                  source={images.home}
+                  tintColor="#151312"
+                  className="size-5"
+                />
+                <Text>Home</Text>
+              </ImageBackground>
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ title: "Saved", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+    </Tabs>
+  );
 }
 
-export default _layout
+export default _layout;
